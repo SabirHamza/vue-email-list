3d1423b085cc028e493e7cb0a27d8c442a31256c
SabirHamza <h.sabir.ma@gmail.com>
2023-02-20 19:57:53 +0100

    init project

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const {createApp} = Vue
+
+const app = createApp({
+     Data(){
+        return{
+            mails: []
+        }
+    },
+
+    mounted (){
+        for(let i = 0; i < 10; i++){
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(result => {
+                console.log(result.data.response);
+            })
+        }
+    }
+})
