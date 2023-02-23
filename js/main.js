1430ce50665a86779065193f924aee224cf69353
SabirHamza <h.sabir.ma@gmail.com>
2023-02-23 19:22:47 +0100

    Email list

--- js/main.js
+++ js/main.js
@@ -11,9 +11,13 @@ const app = createApp({
         for(let i = 0; i < 10; i++){
             axios
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(result => {
+            .then((result) => {
                 console.log(result.data.response);
+
+                this.mails.push(result.data.response);
             })
         }
     }
 })
+
+app.mount('#root')
